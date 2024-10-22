3e8c44c5959ae6459b591dacdd726a4eb56a857c
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-10-22 20:07:11 +0200

    Lesson 02

---
+++ lesson_02/08_complex_sets.py
@@ -0,0 +1,7 @@
+# Tuples embedded into a dictionary
+# Create a dictionary
+d = {"a": (1, 2, 3), "b": (4, 5, 6), "c": (7, 8, 9)}
+print(d)
+# List of tuples
+l = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+print(l)
